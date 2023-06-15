2550d264755c5487bf216347ea7d6abc6fef09e3
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2023-06-14 21:54:23 -0300

    Solution with interface and abstract class

--- src/model/entities/Circle.java
+++ src/model/entities/Circle.java
@@ -2,7 +2,7 @@ package model.entities;
 
 import model.enums.Color;
 
-public class Circle extends Shape {
+public class Circle extends AbstractShape {
 
 	private Double radius;
 
@@ -10,15 +10,12 @@ public class Circle extends Shape {
 		super(color);
 		this.radius = radius;
 	}
-
 	public Double getRadius() {
 		return radius;
 	}
-
 	public void setRadius(Double radius) {
 		this.radius = radius;
 	}
-
 	@Override
 	public double area() {
 		return Math.PI * radius * radius;
